fc47be444e9c3071b506aa6161e621392ba30462
Vincent Fong <vfong00@stuy.edu>
2020-01-15 11:37:44 -0500

    structure hopefully idk

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include "single.h"
+#include "parse.h"
+
+int main() {
+  return 0;
+}

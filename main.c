7b6ceb5057070fb4ba1d9f025dc3fddd7ec8b93e
Vincent Fong <vfong00@stuy.edu>
2020-01-19 09:34:11 -0500

    im just trying to fix stuff what

--- main.c
+++ main.c
@@ -13,5 +13,34 @@
 #include "parse.h"
 
 int main() {
+  printf("-----------------------------------------\n");
+  printf("Welcome to the single player QuizWiz mode!\n");
+  printf("-----------------------------------------\n");
+
+  // Receive username
+  char username[16];
+  printf("Enter username: ");
+  fgets(username, 16, stdin);
+  username[strlen(username)-1] = '\0';
+  printf("Welcome %s!\n", username);
+
+  // Sets up semaphores
+  us.val = 1;
+  sb.sem_num = 0;
+  sb.sem_op = -1;
+  createSemaphore();
+
+  // receive input of number of questions desired
+  int databaseMax = getMaxQuestions(f);
+  printf("There are currently %d questions in the database\n", databaseMax);
+  printf("Enter number of questions desired: ");
+  char input[8];
+  fgets(input, 8, stdin);
+  int n = atoi(input);
+  int score = singleGame(n);
+
+  updateLeaderboard(username, score);
+  viewLeaderboard();
+  removeSemaphore();
   return 0;
 }

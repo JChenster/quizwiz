bb06ded3bd1677d002ac45c6061a25542c3fdbcc
Vincent Fong <vfong00@stuy.edu>
2020-01-19 09:38:06 -0500

    ????

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include "single.h"
+#include "parse.h"
+
+int main() {
+  printf("-----------------------------------------\n");
+  printf("Welcome to the single player QuizWiz mode!\n");
+  printf("-----------------------------------------\n");
+
+  // Receive username
+  char username[16];
+  printf("Enter username: ");
+  fgets(username, 16, stdin);
+  username[strlen(username)-1] = '\0';
+  printf("Welcome %s!\n", username);
+
+  // Sets up semaphores
+  us.val = 1;
+  sb.sem_num = 0;
+  sb.sem_op = -1;
+  createSemaphore();
+
+  // receive input of number of questions desired
+  int databaseMax = getMaxQuestions(f);
+  printf("There are currently %d questions in the database\n", databaseMax);
+  printf("Enter number of questions desired: ");
+  char input[8];
+  fgets(input, 8, stdin);
+  int n = atoi(input);
+  int score = singleGame(n);
+
+  updateLeaderboard(username, score);
+  viewLeaderboard();
+  removeSemaphore();
+  return 0;
+}
--- makefile
+++ makefile
@@ -1,8 +1,14 @@
-all: single.o parse.o
-	gcc -o program single.o parse.o
+all: main.o single.o multi.o parse.o
+	gcc -o program single.o multi.o parse.o 
+
+main.o: main.c single.h multi.h parse.h
+	gcc -c main.c
 
 single.o: single.c single.h
 	gcc -c single.c
+
+multi.o: multi.c multi.h
+	gcc -c multi.c
 
 parse.o: parse.c parse.h
 	gcc -c parse.c
